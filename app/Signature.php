019fdf65efc32e289c76e8747ad5d01697297cec
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2019-10-04 17:21:15 +0700

    reformat code to php psr

--- app/Signature.php
+++ app/Signature.php
@@ -6,14 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class signature extends Model
 {
-    public function scopeIgnoreFlagged($query){
+    public function scopeIgnoreFlagged($query)
+    {
         return $query->where('flagget_at', null);
     }
-    public function flag(){
-    return $this->update(['flagged_at' => \Carbon\Carbon::now()]);
+
+    public function flag()
+    {
+        return $this->update(['flagged_at' => \Carbon\Carbon::now()]);
     }
+
     public function getAvatarAttribute()
     {
-    return sprintf('https://www.gravatar.com/avatar/%s?s=100', md5($this->email));
+        return sprintf('https://www.gravatar.com/avatar/%s?s=100', md5($this->email));
     }
 }
