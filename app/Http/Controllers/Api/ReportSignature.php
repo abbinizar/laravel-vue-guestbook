89623782a799059d5812eb15866658e3667209e7
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2019-10-04 17:20:56 +0700

    reformat code to php psr

--- app/Http/Controllers/Api/ReportSignature.php
+++ app/Http/Controllers/Api/ReportSignature.php
@@ -7,7 +7,8 @@ use App\Http\Controllers\Controller;
 
 class ReportSignature extends Controller
 {
-    public function update(Signature $signature){
+    public function update(Signature $signature)
+    {
         $signature->flag();
         return $signature;
     }
